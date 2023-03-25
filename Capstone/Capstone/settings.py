a2fa00ecc58eb18ed0b0207fd254b87a5de48c20
Kamil Sidor <kamil_sidor@o2.pl>
2023-03-25 17:04:21 +0100

    'xxx'

--- Capstone/Capstone/settings.py
+++ Capstone/Capstone/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sgiyb)i65cx!+bc9chn6_$fw2b3$h5^hken+_i$n5_w#^#8pbx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'gwybodaeth.onrender.com',
+]
 
 
 # Application definition
@@ -119,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
